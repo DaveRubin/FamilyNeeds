8a493badc851adc41b0b027fc90307e89b886358
David Rubin <dudrubin@gmail.com>
2018-05-26 19:34:16 +0300

    linting changes

--- app/containers/SurveyPage/index.js
+++ app/containers/SurveyPage/index.js
@@ -7,7 +7,6 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { FormattedMessage } from 'react-intl';
 
-import H1 from 'components/H1';
 import messages from './messages';
 import Survey from './Containers/Sruvey';
 import { SurveyHeader } from './styles';
